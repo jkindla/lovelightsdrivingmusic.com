053900fb86f6adac03e5fb2edf945a3ff00ecff3
jkindla <82672312+jkindla@users.noreply.github.com>
2022-11-20 19:56:27 -0600

    edit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -38,100 +39,37 @@ $('a[href*="#"]')
 
 
 
-const slide = {
-    main : null,
-    elementImg : null,
-    imgSelected : 0,
-    nextSlide: function (){
-        if (this.imgSelected != null)
-        {
-            if (this.imgSelected < (this.elementImg.length - 1))
-            {
-                this.imgSelected++;
-                this.normalizeSlide();
-            }
-        }
-    },
-    prevSlide: function (){
-        if (this.imgSelected != null)
-        {
-            if (this.imgSelected > 0)
-            {
-                this.imgSelected--;
-                this.normalizeSlide();
-            }
-        }
-    },
-    normalizeSlide: function (){
 
-        for (num = 0; num < this.elementImg.length; num++)
-        {
-            this.elementImg[num].classList.remove("hideLeft","prevLeftSecond","prev","selected","next","nextRightSecond","hideRight");
-        }
-
-        this.elementImg[this.imgSelected].classList.add("selected");
-
-        if (this.imgSelected > 2)
-        {
-            this.elementImg[this.imgSelected-2].classList.add("hideLeft");
-            this.elementImg[this.imgSelected-2].classList.add("prevLeftSecond");
-            this.elementImg[this.imgSelected-1].classList.add("prev");
-        }
-        else if (this.imgSelected > 1)
-        {
-            this.elementImg[this.imgSelected-2].classList.add("prevLeftSecond");
-            this.elementImg[this.imgSelected-1].classList.add("prev");
-        }
-        else if (this.imgSelected > 0)
-        {
-            this.elementImg[this.imgSelected-1].classList.add("prev");
-        }
-
-        if ((this.imgSelected + 3) < this.elementImg.length)
-        {
-            this.elementImg[this.imgSelected+3].classList.add("hideRight");
-            this.elementImg[this.imgSelected+2].classList.add("nextRightSecond");
-            this.elementImg[this.imgSelected+1].classList.add("next");
-        }
-        else if ((this.imgSelected + 2) < this.elementImg.length)
-        {
-            this.elementImg[this.imgSelected+2].classList.add("nextRightSecond");
-            this.elementImg[this.imgSelected+1].classList.add("next");
-        }
-        else if((this.imgSelected + 1) < this.elementImg.length)
-        {
-            this.elementImg[this.imgSelected+1].classList.add("next");
-        }
+$(window).scroll(function() {
+    var header = $(document).scrollTop();
+    var headerHeight = $(".header").outerHeight();
+    var firstSection = $(".main-container section:nth-of-type(1)").outerHeight();
+    if (header > headerHeight) {
+      $(".header").addClass("fixed");
+    } else {
+      $(".header").removeClass("fixed");
     }
-}
-
-window.onload = () => {
-
-    slide.main = document.getElementById("carousel");
-    slide.elementImg = slide.main.getElementsByClassName("slideImg");
-
-    for (num = 0; num < slide.elementImg.length; num++)
-    {
-        slide.elementImg[num].setAttribute("img-number", num);
-        
-        slide.elementImg[num].addEventListener("click", (event) => {
-            slide.imgSelected = parseInt(event.target.parentElement.getAttribute("img-number"));
-            slide.normalizeSlide();
-        });
-
-        if (slide.elementImg[num].classList.contains("selected"))
-        {
-            slide.imgSelected = num;
-        }
+    if (header > firstSection) {
+      $(".header").addClass("in-view");
+    } else {
+      $(".header").removeClass("in-view");
     }
-
-    document.getElementById("prev").addEventListener("click", () => {slide.prevSlide()});
-    document.getElementById("next").addEventListener("click", () => {slide.nextSlide()});
-
-}
+  });
 
 
 
 
 
+  
+
+
+
+
+
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
 
